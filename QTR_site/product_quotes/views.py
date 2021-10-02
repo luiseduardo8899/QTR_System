6ea133667b180837c8f94da0e8b81d05461f2777
luiseduardo8899 <luiseduardo8899@gmail.com>
2021-10-02 11:11:59 -0700

    compete initial models to test quoting system, tested models can be crated on admin interface

--- QTR_site/product_quotes/views.py
+++ QTR_site/product_quotes/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect, HttpResponse
+from dictionary.models import Entry
+from dictionary.utils import * #to get random entries, quizes
+from userinfo.utils import * #to get user profile and stats
+from django.template import loader
+from django.http import Http404
+from django.utils.decorators import method_decorator
+from django.contrib.auth.decorators import login_required
+from django.urls import reverse
+from random import *
+from django.core.mail import send_mail
 
-# Create your views here.
+#def create_quote(request):
+#def create_license(request):
